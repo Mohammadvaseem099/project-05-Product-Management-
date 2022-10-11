d5049a93bde5488eefcfe0108ca0226662ed4dd8
RaM <ramsalunkhe6@gmail.com>
2022-10-11 11:14:04 +0530

    package.json added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const mongoose = require("mogoose")
+const mongoose = require("mongoose")
 const route = require('../src/routes/route');
 const app =express();
 
@@ -9,12 +9,12 @@ app.use(express.json());
 let url = "mongodb+srv://saurav438c:Bharat123@cluster0.ueecgjt.mongodb.net/project5"
 let port = process.env.PORT || 3000;
 
-mongoose.connect(url, {userNewUrlParser: true})
-.then(()=> console.log("MongoDB is connected...."))
+mongoose.connect(url, {useNewUrlParser: true})
+.then(()=> console.log("MongoDB is connected..."))
 .catch(err => console.log(err));
 
 app.use("/", route);
 
 app.listen(port, ()=>{
-    console.log("Express app is running on port" +port);
+    console.log("Express app is running on port " +port);
 })
