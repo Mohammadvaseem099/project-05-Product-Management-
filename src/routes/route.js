d3eea0255bb9ddaab7dd23fef3e15a3d9ede393f
Mohammad Waseem <mohammadvaseem099@gmail.com>
2022-10-13 00:16:12 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controller/usercontroller')
 const auth = require('../middleware/auth')
+const productController = require('../controller/productController')
 
 
 
@@ -11,5 +12,7 @@ router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.get('/user/:userId/profile',auth.authenticate, userController.getUser)
 router.put('/user/:userId/profile',auth.authenticate, auth.authorization, userController.updateUser)
+
+router.post("/products", productController.createProduct)
 
 module.exports = router
