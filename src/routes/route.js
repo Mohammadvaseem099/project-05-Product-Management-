002b2ef17cf49637334cf3fdf92d91e2807cab3a
Saurav438c <saurav438c@gmail.com>
2022-10-17 11:34:10 +0530

    cart

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require("../controller/usercontroller");
 const auth = require("../middleware/auth");
 const productController = require("../controller/productController");
+const cartController = require('../controller/cartController')
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
@@ -14,5 +15,14 @@ router.get("/products", productController.getProduct);
 router.get("/products/:productId", productController.getProductById);
 router.put("/products/:productId", productController.updateProduct);
 router.delete("/products/:productId", productController.deleteProduct);
+
+
+router.post('/users/:userId/cart', auth.auth, cartController.createCart)
+router.put('/users/:userId/cart', auth.auth, cartController.updateCart)
+router.get('/users/:userId/cart', auth.auth, cartController.getById)
+router.delete('/users/:userId/cart', auth.auth, cartController.deleteCart)
+
+
+
 
 module.exports = router;
